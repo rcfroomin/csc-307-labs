9a3d0b2688724deceaa6147c1a82d36630ca5bed
rcfroomin <rcfroomin@gmail.com>
2024-04-26 16:17:46 -0700

    ie4 progress completed - again

---
+++ packages/expressjs-with-mongoose-localdb_rf/backend_with_db.js
@@ -0,0 +1,58 @@
+import express from "express";
+import cors from "cors";
+
+import userService from "./services/user-service.js";
+
+const app = express();
+const port = 8000;
+// test comment
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+  const name = req.query["name"];
+  const job = req.query["job"];
+  userService
+    .findUserByNameAndJob(name, job)
+    .then((result) => {
+      res.send({ users_list: result });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("An error ocurred in the server.");
+    })
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  userService.findUserById(id).then((result) => {
+    if (result === undefined || result === null)
+      res.status(404).send("Resource not found.");
+    else res.send({ users_list: result });
+  });
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  userService.deleteUserById(id).then((result) => {
+    if (result === undefined || result === null)
+      res.status(404).send("Resource not found.");
+    else res.status(204).end();
+  });
+});
+
+app.post("/users", (req, res) => {
+  const user = req.body;
+  userService.addUser(user).then((savedUser) => {
+    if (savedUser) res.status(201).send(savedUser);
+    else res.status(500).end();
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
